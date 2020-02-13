1074184b951225872047f0426d23cb33901335be
Amie Corso <acorso@ripple.com>
2020-02-13 14:00:32 -0800

    initial

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import xpring.io.xrpl.Wallet;
+
+public class Main {
+    Wallet seedWallet = new Wallet("snRiAJGeKCkPVddbjB3zRwwoiYDBm1M");
+
+    public static void main(String[] args) {
+        System.out.print("Hello from main in Main in Sandbox4J!");
+    }
+}
